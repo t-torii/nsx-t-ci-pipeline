f674f32fa248444cc2616260612b721333a2db99
Torii <Takashi.Torii@emc.com>
2018-11-07 12:50:38 +0900

    wipe test

--- tasks/wipe-env/task.sh
+++ tasks/wipe-env/task.sh
@@ -13,7 +13,7 @@ export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > $GOVC_TLS_CA_CERTS
 
 source "${root}/nsx-t-ci-pipeline/functions/check_opsman_available.sh"
-
+echo "start wipe"
 opsman_available=$(check_opsman_available $OPSMAN_DOMAIN_OR_IP_ADDRESS)
 if [[ $opsman_available == "available" ]]; then
 
